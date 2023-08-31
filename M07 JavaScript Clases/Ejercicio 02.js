a9761f57fa15d331f6b35bd7c261267a4fe684b9
unknown <julietajacquat@gmail.com>
2023-08-30 23:50:11 -0300

    modulo 7 resuelto

--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -6,6 +6,20 @@ class Persona {
    // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
    // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
    // sus valores.
+   constructor (nombre, apellido, edad, domicilio){
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.edad = edad;
+      this.domicilio = domicilio;
+   }
+   detalle () {
+      return { 
+               nombre: this.nombre,
+               apellido: this.apellido,
+               edad: this.edad,
+               domicilio: this.domicilio
+      }
+      }
 }
 
 function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
@@ -13,6 +27,9 @@ function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
    // Recibirás las propiedades por parámetro.
    // Retornar la instancia creada.
    // Tu código:
+   var instancia = new Persona (nombre, apellido, edad, domicilio) //var y let es lo mismo? 
+   //new es la manera de instanciar una clase, con esos datos recibidos como parametros
+   return instancia
 }
 
 function agregarMetodo() {
@@ -20,6 +37,11 @@ function agregarMetodo() {
    // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
    // Ejemplo: "Juan, 22 años".
    // Tu código:
+   Persona.prototype.datos = function(){ //se usar prototype porque estamos creando el mettodo por fuera de la clase 
+      return this.nombre + ', ' + this.edad + ' años' //seria lo mismo crear datos () {return this.nombre + ', ' + this.edad + ' años'} dentro de la clase Persona
+      //otra manera de escribir la ultima linea return '${this.nombre}, ${this.edad} años'
+   }
+   
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
